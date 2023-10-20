95a9bcdbcb0f071d9a98f8e06fb44ec41b81a36f
E2-DESD-Surya <surajraj8433@gmail.com>
2023-10-20 08:25:42 +0530

    new commit

--- A4_02.c
+++ A4_02.c
@@ -1,15 +1,37 @@
 #include<stdio.h>
-#include"fact.h"
+int calculatePower(int base,int index)
+ {
+ int pow = 1;
+  if(base == 1)
+   {
+    printf("power of 1 is always 1:\n");
+	return 1;
+   }
+   else if(index == 1)
+   {
+     return base;
+   }
+   else
+   {
+   
+	 for(int i = 1; i <= index; i++)
+	 {
+      pow *= base ;
+      
+	 }
+	 return pow;
+   }
+
+ }
 
 int main(void)
 {
-      int num,f;
-       printf("Enter the number: ");
-	   scanf("%d",&num);
-	   f = fact(num);
-	   printf("fact of %d!= %d\n",num , f);
+ int base,power;
+ printf("Enter base and power:\n");
+ scanf("%d %d",&base,&power);
+ int pow = calculatePower(base,power);
+ printf("power = %d ",pow);
 
 
-   return 0;
-
+return 0;
 }
